8b8fe1babff53149900a7689b8ea5ab58a936012
Uzmeyer <jakob.goessinger@gmail.com>
2020-08-13 21:36:10 +0200

    Additions to serialplot

--- Arduino/Vibration Analysis/src/main.cpp
+++ Arduino/Vibration Analysis/src/main.cpp
@@ -1,49 +1,88 @@
 #include <Arduino.h>
-#include <Adafruit_MPU6050.h>
+#include <I2Cdev.h>
+#include "MPU6050.h"
 
+// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
+// is used in I2Cdev.h
+#if (I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE) && !defined (PARTICLE)
+    #include "Wire.h"
+#endif
+
+// class default I2C address is 0x68
+// specific I2C addresses may be passed as a parameter here
+// AD0 low = 0x68 (default for InvenSense evaluation board)
+// AD0 high = 0x69
+
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+uint8_t sbyte1 = 0b11111111;
+uint8_t sbyte2 = 0b01010101;
+
+// uncomment "OUTPUT_READABLE_ACCELGYRO" if you want to see a tab-separated
+// list of the accel X/Y/Z and then gyro X/Y/Z values in decimal. Easy to read,
+// not so easy to parse, and slow(er) over UART.
+#define OUTPUT_READABLE_ACCELGYRO
+
+// uncomment "OUTPUT_BINARY_ACCELGYRO" to send all 6 axes of data as 16-bit
+// binary, one right after the other. This is very fast (as fast as possible
+// without compression or data loss), and easy to parse, but impossible to read
+// for a human.
+#define OUTPUT_BINARY_ACCELGYRO
+
+#define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
+
+MPU6050 accelgyro;
 long data = 0;
 long i = 0;
-
-Adafruit_MPU6050 mpu;
-Adafruit_Sensor *mpu_temp, *mpu_accel, *mpu_gyro;
+bool blinkState = false;
 
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(2000000);
   Serial.print("Starting...\n");
-  if (!mpu.begin()) {
-    Serial.println("Failed to find MPU6050 chip");
-    while (1) {
-      delay(10);
-    }
-  }
+  // join I2C bus (I2Cdev library doesn't do this automatically)
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+  
+  Serial.println("Initializing I2C devices...");
+  accelgyro.initialize();
+  Serial.println("Testing device connections...");
+  Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+  Serial.print("Current Bandwith config: ");
+  Serial.println(accelgyro.getDLPFMode());
+  accelgyro.setDLPFMode(MPU6050_DLPF_BW_256);
+  Serial.print("Current Accel FullScale config: ");
+  Serial.println(accelgyro.getFullScaleAccelRange());
+
+
+  pinMode(LED_PIN, OUTPUT);
+
   randomSeed(analogRead(0));
-  Serial.println("MPU6050 Found!");
-  mpu_temp = mpu.getTemperatureSensor();
-  mpu_temp->printSensorDetails();
-
-  mpu_accel = mpu.getAccelerometerSensor();
-  mpu_accel->printSensorDetails();
-
-  mpu_gyro = mpu.getGyroSensor();
-  mpu_gyro->printSensorDetails();
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  sensors_event_t accel;
-  mpu_accel->getEvent(&accel);
+  accelgyro.getAcceleration(&ax, &ay, &az);
 
-  /* Display the results (acceleration is measured in m/s^2) */
-  Serial.print(i);
-  Serial.print(",");
-  Serial.print(accel.acceleration.x);
-  Serial.print(",");
-  Serial.print(accel.acceleration.y);
-  Serial.print(",");
-  Serial.print(accel.acceleration.z);
-  Serial.print("\n");
+
+  #ifdef OUTPUT_BINARY_ACCELGYRO
+        Serial.write(sbyte1); 
+        Serial.write(sbyte2);
+        Serial.write((uint8_t)(ax >> 8)); Serial.write((uint8_t)(ax & 0xFF));
+        Serial.write((uint8_t)(ay >> 8)); Serial.write((uint8_t)(ay & 0xFF));
+        Serial.write((uint8_t)(az >> 8)); Serial.write((uint8_t)(az & 0xFF));
+        Serial.write((uint8_t)(gx >> 8)); Serial.write((uint8_t)(gx & 0xFF));
+        Serial.write((uint8_t)(gy >> 8)); Serial.write((uint8_t)(gy & 0xFF));
+        Serial.write((uint8_t)(gz >> 8)); Serial.write((uint8_t)(gz & 0xFF));
+  #endif
+  
   i++;
+  blinkState = !blinkState;
+  digitalWrite(LED_PIN, blinkState);
   delay(100);
 }
--- Python/serialplot.py
+++ Python/serialplot.py
@@ -8,6 +8,9 @@ import serial.tools.list_ports
 
 from threading import Thread
 import time
+import copy
+import collections
+import struct
 
 
 class SerialPlot:
@@ -17,11 +20,29 @@ class SerialPlot:
         self.plotLength = plotLength
         self.dataNumBytes = dataNumBytes
         self.numPlots = numPlots
+        if dataNumBytes == 2:
+            self.dataType = 'h'     # 2 byte integer
+        elif dataNumBytes == 4:
+            self.dataType = 'f'     # 4 byte float
+        self.data = []
+        self.data.append(collections.deque([0] * plotLength, maxlen=plotLength))  #time
+        for i in range(self.numPlots):
+            self.data.append(collections.deque([0] * plotLength, maxlen=plotLength))  #data
+        self.rawData = bytearray(numPlots * dataNumBytes + 2 + 2)  #buffer for databytes + startbytes + timebytes
+        self.recBuffer = collections.deque(maxlen=64)
         self.serial = serial.Serial()
         self.isSerialOpen = False
+        self.isRun = False
+        self.isReceiving = True
         self.serialPortNames = []
         self.scanports()
         self.thread = None
+        self.sbyte1Found = False
+        self.sbyte2Found = False
+        self.messageLength = self.dataNumBytes * numPlots + 2
+        self.message = bytearray(self.messageLength)
+        self.bytesCollected = 0
+        self.messages = []
 
         self.setbaud(serialBaud)
         self.setserial(serialPort)
@@ -81,8 +102,56 @@ class SerialPlot:
             print("{}: {} [{}]".format(port, desc, hwid))
             self.serialPortNames.append(port)
 
+    def getserialdata(self):
+        while len(self.recBuffer) > 0:
+            byte = self.recBuffer.popleft()
+            if not self.sbyte1Found and not self.sbyte2Found:
+                if byte != 255:
+                    continue
+                else:
+                    self.sbyte1Found = True
+                    continue
+            elif self.sbyte1Found and not self.sbyte2Found:
+                if byte == 85:
+                    self.sbyte2Found = True
+                    continue
+                else:
+                    self.sbyte1Found = False
+                    continue
+            elif self.bytesCollected < self.messageLength:
+                self.message[self.bytesCollected] = byte
+                self.bytesCollected += 1
+                continue
+            else:
+                print(self.message)
+                self.messages.append(self.message.copy())
+                self.bytesCollected = 0
+                self.sbyte1Found = False
+                self.sbyte2Found = False
+
     def serialStart(self):
-        print("test")
+        if self.thread is None:
+            self.isRun = True
+            self.thread = Thread(target=self.datareadthread)
+            self.thread.start()
+            while self.isReceiving != True:
+                time.sleep(0.1)
+
+    def datareadthread(self):
+        time.sleep(1)
+        self.serial.reset_input_buffer()
+        while self.isRun:
+            self.serial.readinto(self.rawData)
+            self.isReceiving = True
+            privateData = copy.deepcopy(self.rawData)
+            for byte in privateData:
+                self.recBuffer.append(copy.deepcopy(byte))
+
+    def close(self):
+        self.isRun = False
+        self.thread.join()
+        self.serial.close()
+        print("Data collection stopped\n")
 
 
 if __name__ == "__main__":
@@ -95,6 +164,13 @@ if __name__ == "__main__":
 
     if len(portnames) > 1:
         print("Found serial device\n")
-        s = SerialPlot(serialPort=portnames[1])
+        s = SerialPlot(serialPort=portnames[1], serialBaud=2000000, numPlots=3)
     else:
         print("No serial devices found\n")
+
+    s.serialStart()
+    t_end = time.time() + 10
+    while t_end > time.time():
+        s.getserialdata()
+    s.close()
+    print("done")
